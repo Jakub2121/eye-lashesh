1de6590c647cb563cdbf9fac83ab2bb36f75db54
Jakub2121 <72815913+Jakub2121@users.noreply.github.com>
2026-07-31 22:27:20 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'beauty-studio-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
